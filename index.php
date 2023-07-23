3e305ecc4bf3e10ad1316e14f5820a1fe397ce74
Mamad <naufaloke679@gmail.com>
2023-07-23 16:29:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rizqika Ibnu K</title>
+    <script src="https://kit.fontawesome.com/e674bba739.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <section id="header">
+        <div class="navbar-title">
+            <h3 class="title-first-name">
+                <a href="#">Rizqika</a>
+            </h3>
+            <h3 class="title-last-name">
+                Ibnu K
+            </h3>
+        </div>
+        <div>
+            <ul class="navbar-menu">
+                <li><a class="active" href="#introduction">&lt; Service &gt;</a></li>
+                <li><a class="" href="#latest-works">Works</a></li>
+                <li><a class="" href="#testimonial">About Me</a></li>
+                <li><a class="" href="#footer">Contacts</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="social-media">
+                <li>
+                    <i class="fa-brands fa-linkedin-in"></i>
+                    <a href="https://www.linkedin.com/in/rizqika-ibnu-231714218/" target="_blank">LinkedIn</a>
+                </li>
+                <li>
+                    <i class="fa-brands fa-github"></i>
+                    <a href="https://github.com/riz081" target="_blank">Github</a>
+                </li>
+                <li>
+                    <i class="fa-regular fa-envelope"></i>
+                    <a href="#footer">Email</a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section id="content-body">
+        <div class="body-part-1">
+            <div class="developer-intro">
+                <p>Full-Stack Developer</p>
+            </div>
+            <div class="body-title">
+                <!-- <h1>Talk is cheap<br>Show me the code</h1> -->
+                <h1>I’m not a great programmer<br> I’m just a good programmer <br> with great habits</h1>
+                <p>Programming isn't about what you know,<br> it's about what you can figure out.</p>
+            </div>
+            <div class="body-tail">
+                <h1>2</h1>
+                <p>YEARS<br>EXPERIENCE</p>
+                <h1>7</h1>
+                <p>PROJECTS<br>COMPLETED</p>
+            </div>
+        </div>
+        <div class="body-part-2">
+            <div class="hoodie-guy-animation-class">
+                <div class="hoodie-guy"></div>
+                <div class="circle">
+                    <span style="--i:1;"><img src="assets/SVGIcons/python.svg" height="75px"></span>
+                    <span style="--i:3;"><img src="assets/SVGIcons/react-2.svg" height="75px"></span>                    
+                    <span style="--i:5;"><img src="assets/SVGIcons/laravel-2.svg" height="75px"></span>
+                    <span style="--i:7;"><img src="assets/SVGIcons/codeigniter.svg" height="85px"></span>
+                    <span style="--i:9;"><img src="assets/SVGIcons/figma.svg" height="75px"></span>                 
+                    <span style="--i:11;"><img src="assets/SVGIcons/MySQL-Dark.svg" height="75px"></span>                    
+                    <span style="--i:13;"><img src="assets/SVGIcons/Github-Dark.svg" height="75px"></span>
+                </div>
+            </div>
+            <div class="background-circle"></div>
+        </div>
+    </section>
+    <section id="introduction">
+        <div class="cards">
+            <div class="design-card active">
+                <div>
+                    <h3>UI/UX Design</h3>
+                    <i class="fa-solid fa-wand-magic-sparkles"></i>
+                </div>
+                <p>Create Design Products With Unique Ideas that Matters</p>
+            </div>
+            <div class="design-card">
+                <div>
+                    <h3>Frontend Development</h3>
+                    <i class="fa-solid fa-code"></i>
+                </div>
+                <p>Make Your Design Like a Magic</p>
+            </div>
+            <div class="design-card">
+                <div>
+                    <h3>Backend Development</h3>
+                    <i class="fa-solid fa-terminal"></i>
+                </div>
+                <p>Building the System Application Like Witchcraft</p>
+            </div>
+        </div>
+        <div class="introduction-text">
+            <p>Introduce My Self as UI/UX Designer</p>
+            <h2>Hello! I'm Rizqika Ibnu K</h2>
+            <h4>Create User Experiences With Attractive Interaction Designs</h4>
+            <p>
+                Saya seorang desainer UI/UX yang menciptakan pengalaman pengguna yang interaktif dan sederhana untuk aplikasi web dan seluler. 
+                Saya menggunakan keahlian saya dalam riset pengguna, pemikiran desain, dan pembuatan prototipe untuk membuat antarmuka mudah digunakan dan dinavigasi. 
+                Sehingga menjadikannya pengalaman yang sangat berkesan dan menyenangkan bagi pengguna
+            </p>
+        </div>
+        <div class="introduction-text" style="display: none;">
+            <p>Introduce My Self as Frontend Developer</p>
+            <h2>Hello! I'm Rizqika Ibnu K</h2>
+            <h4> Attractive Web and Mobile Views Are Not Magic</h4>
+            <p>
+                Sebagai pengembang frontend, saya membuat halaman web yang responsif, sederhana, dan menarik. 
+                Saya menggunakan keahlian saya dalam HTML, CSS, JavaScript untuk mengimplementasikan antarmuka pengguna situs web, dan React Native untuk mengimplementasikan antarmuka pengguna seluler.
+            </p>
+        </div>
+        <div class="introduction-text" style="display: none;">
+            <p>Introduce My Self as UI/UX Backend Developer</p>
+            <h2>Hello! I'm Rizqika Ibnu K</h2>
+            <h4>Coding is so Powerfull Magic</h4>
+            <p>
+                Sebagai pengembang backend, saya membuat aplikasi yang kompleks dan dapat diskalakan untuk menangani logika dan data di balik aplikasi.
+                Saya menggunakan keahlian saya di Laravel Framework untuk membangun API untuk mengelola data yang akan saya gunakan di aplikasi yang saya bangun, 
+                sehingga sistem backend menjadi aman dan efisien.
+            </p>
+        </div>
+    </section>
+    <section id="latest-works">
+        <div class="left-project">
+            <div class="latest-work-title">
+                <h3>Latest Works</h3>
+                <p>Perfect solutions for digital experience</p>
+            </div>
+
+            <div class="project-card project-card-1">
+                <div class="title">
+                    <h3>Agecare</h3>
+                    <div class="tech-stack">
+                        <p>Codeigniter</p>
+                        <p>Bootstrap</p>
+                    </div>
+                </div>
+                <img src="assets/Projects/agecare.png" alt="" class="project-img-2">
+            </div>
+
+            <div class="all-projects">
+                <a href="">
+                    <h3>REPOSITORY PROJECTS</h3>
+                </a>
+                <p>* See more project on my repository Github <br> <a href="https://github.com/riz081?tab=repositories">Click Here</a>
+                </p>
+            </div>
+        </div>
+        <div class="right-project">
+            <div class="project-card project-card-2">
+                <div class="title">
+                    <h3>BCCare</h3>
+                    <div class="tech-stack">
+                        <p>React Native</p>
+                        <p>Laravel</p>
+                    </div>
+                </div>
+                <img src="assets/Projects/bccare.png" alt="" class="project-img-1">
+            </div>
+            <div class="project-card project-card-3">
+                <div class="title">
+                    <h3>Flucht</h3>
+                    <div class="tech-stack">
+                        <p>React Native</p>
+                        <p>Laravel</p>
+                    </div>
+                </div>
+                <img src="assets/Projects/flucht2.png" alt="" class="project-img-3">
+            </div>
+        </div>
+    </section>
+    <section id="testimonial">
+        <div class="testimonial-title">
+            <h2>About Me</h2>
+        </div>
+        <div class="testimonial-card">
+            <h4>Biography</h4>
+            <p>Name     : Rizqika Ibnu Khattab</p>
+            <p>Age      : 23 Year's old</p>
+            <p>Phone    : 0895633150324</p>
+            <p>Email    : rizqikaibnu@gmail.com</p>
+            <p>Education: Final Year Student of Information Systems </p>
+            <i class="fa-solid fa-user"></i>
+        </div>
+
+        <div class="testimonial-card" style="display: none;">
+            <h4>Let's see. I am reliable and consistent in taking responsibility for what I do. I am someone who wants to keep learning and adapting to ever-evolving technology, appreciating the hard work of others is my expression of motivating the efforts of others.</h4>
+            <p>"Let's see. Saya merupakan seorang yang konsisten dan diandalkan dalam bertanggung jawab <br> atas apa yang saya kerjakan.
+                Saya adalah seseorang yang ingin terus belajar dan <br> beradaptasi dengan teknologi yang terus berkembang, 
+                serta menghargai kerja keras orang lain <br> adalah ekspresi saya untuk memotivasi usaha orang lain."</p>
+            <i class="fa-solid fa-quote-right"></i>
+        </div>
+        <div class="testimonial-card" style="display: none;">
+            <h4>Work Experience</h4>
+            <p>*)IT - Full Stack Developer in PT. Berkat Cipta Logistics while 3 Months (Internship)</p>
+            <i class="fa-solid fa-briefcase"></i>
+        </div>
+        <div class="client-profile-card">
+            <div class="single-profile-card profile-card-active">
+                <img src="assets/client-profile.png" alt="">
+                <div>
+                    <h4>Biography</h4>
+                    <p>Rizqika Ibnu K, <a href="">Wiser</a></p>
+                </div>
+            </div>
+            <div class="single-profile-card">
+                <img src="assets/client-profile.png" alt="">
+                <div>
+                    <h4>Describe Me</h4>
+                    <p>Rizqika Ibnu K, <a href="">Wiser</a></p>
+                </div>
+            </div>
+            <div class="single-profile-card">
+                <img src="assets/client-profile.png" alt="">
+                <div>
+                    <h4>Experience</h4>
+                    <p>Rizqika Ibnu K, <a href="">Wiser</a></p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section id="footer">
+        <div class="footer-left">
+            <h2>Let's make something amazing together</h2>
+
+            <div class="email-form">
+                <h2>Start by <span>saying hi</span></h2>
+                <form class="formku" action="send.php" method="post">
+                    <input type="email" name="email" id="" placeholder="Email Address" value="">
+                    <input type="text" name="name" id="" placeholder="Your'e Name">
+
+                    <div>
+                        <input type="text" name="message" id="" placeholder="Input Message">
+                        <button type="submit" name="send">Send</button>
+                    </div>
+                </form>
+                
+            </div>
+
+            <div class="footer-title">
+                <h3 class="title-first-name">
+                    Rizqika 
+                </h3>
+                <h3 class="title-last-name">
+                    Ibnu K
+                </h3>
+            </div>
+        </div>
+        <div class="footer-right">
+            <div class="footer-email-intro">
+                <p>Information</p>
+                <h6>East Java, Indonesia, Poss-Code 61262</h6>
+                <h3>rizqikaibnu@gmail.com</h3>
+            </div>
+            <div class="footer-nav-menu">
+                <ul class="footer-menu">
+                    <li><a class="active" href="#introduction">&lt; Service &gt;</a></li>
+                    <li><a class="" href="#latest-works">Works</a></li>
+                    <li><a class="" href="#testimonial">About Me</a></li>
+                    <li><a class="" href="#footer">Contacts</a></li>
+                </ul>
+            </div>
+            <div class="social-icons">
+                <a href="https://www.linkedin.com/in/rizqika-ibnu-231714218/" target="_blank">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </a>
+                <a href="https://github.com/riz081" target="_blank">
+                    <i class="fa-brands fa-github"></i>
+                </a>
+                <a href="https://www.instagram.com/rizqika_i.k/" target="_blank">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="https://www.facebook.com/rizqika.ningendo/" target="_blank">
+                    <i class="fa-brands fa-facebook"></i>
+                </a>
+                <a href="https://wa.me/+62895633150324?text=Hello,%20Traveller" target="_blank">
+                    <i class="fa-brands fa-whatsapp"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+    <script src="script.js"></script>
+</body>
+
+</html>
